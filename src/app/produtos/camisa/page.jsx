13402ffffd1fbb11f77150f833a224635b13d646
gabsmelin <rm550202@fiap.com.br>
2023-10-11 20:32:50 -0300

    Itens da página de camisetas adicionados(não compenentizados)

--- src/app/produtos/camisa/page.jsx
+++ src/app/produtos/camisa/page.jsx
@@ -1,9 +1,35 @@
-
+import Image from "next/image"
 
 export default function Camisa() {
     return(
         <div>
-            
+            <div>
+                <h1>CAMISETAS</h1>
+                <h5>AS CAMISETAS MAIS BONITAS DA QUEBRADA VOCÊ SÓ ENCONTRA AQUI!</h5>
+            </div>
+            <ul>
+                <li>
+                    <Image src="/img/camiseta1.png" alt="Camiseta cargo verde." width={250} height={300}/>
+                    <p>Camiseta Cargo</p>
+                    <p>R$100,00</p>
+                    <p>Camiseta na cor branca, estampada.</p>
+                    <button>Esgotado.</button>
+                </li>
+                <li>
+                    <Image src="/img/camiseta2.png" alt="Camiseta cargo jeans." width={250} height={300}/>
+                    <p>Camiseta Cargo</p>
+                    <p>R$100,00</p>
+                    <p>Camiseta Cargo na cor preta, estampada.</p>
+                    <button>Esgotado.</button>
+                </li>
+                <li>
+                    <Image src="/img/camiseta3.png" alt="Camiseta cargo bege." width={250} height={300}/>
+                    <p>Camiseta Cargo</p>
+                    <p>R$100,00</p>
+                    <p>Camiseta Cargo na cor azul, estampada.</p>
+                    <button>Esgotado.</button>
+                </li>
+            </ul>
         </div>
     )
 }
