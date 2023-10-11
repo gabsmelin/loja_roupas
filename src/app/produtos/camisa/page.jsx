194149fcb80dd4cc9657d7aa84ea21cb26e341f7
gabsmelin <rm550202@fiap.com.br>
2023-10-11 20:36:25 -0300

    Itens da página de meias adicionados(não componentizados)

--- src/app/produtos/camisa/page.jsx
+++ src/app/produtos/camisa/page.jsx
@@ -11,21 +11,21 @@ export default function Camisa() {
                 <li>
                     <Image src="/img/camiseta1.png" alt="Camiseta cargo verde." width={250} height={300}/>
                     <p>Camiseta Cargo</p>
-                    <p>R$100,00</p>
+                    <p>R$79,99</p>
                     <p>Camiseta na cor branca, estampada.</p>
                     <button>Esgotado.</button>
                 </li>
                 <li>
                     <Image src="/img/camiseta2.png" alt="Camiseta cargo jeans." width={250} height={300}/>
                     <p>Camiseta Cargo</p>
-                    <p>R$100,00</p>
+                    <p>R$85,00</p>
                     <p>Camiseta Cargo na cor preta, estampada.</p>
                     <button>Esgotado.</button>
                 </li>
                 <li>
                     <Image src="/img/camiseta3.png" alt="Camiseta cargo bege." width={250} height={300}/>
                     <p>Camiseta Cargo</p>
-                    <p>R$100,00</p>
+                    <p>R$90,00</p>
                     <p>Camiseta Cargo na cor azul, estampada.</p>
                     <button>Esgotado.</button>
                 </li>
--- src/app/produtos/meia/page.jsx
+++ src/app/produtos/meia/page.jsx
@@ -1,9 +1,35 @@
-
+import Image from "next/image"
 
 export default function Meia() {
     return(
         <div>
-            
+             <div>
+                <h1>MEIAS</h1>
+                <h5>AS MEIAS MAIS BONITAS DA QUEBRADA VOCÊ SÓ ENCONTRA AQUI!</h5>
+            </div>
+            <ul>
+                <li>
+                    <Image src="/img/Meia1.png" alt="Meia cargo verde." width={250} height={300}/>
+                    <p>Meia Cargo</p>
+                    <p>R$25,00</p>
+                    <p>Meia na cor branca com preta, estampada.</p>
+                    <button>Esgotado.</button>
+                </li>
+                <li>
+                    <Image src="/img/Meia2.png" alt="Meia cargo jeans." width={250} height={300}/>
+                    <p>Meia Cargo</p>
+                    <p>R$20,00</p>
+                    <p>Meia Cargo na cor preta com branco, estampada.</p>
+                    <button>Esgotado.</button>
+                </li>
+                <li>
+                    <Image src="/img/Meia3.png" alt="Meia cargo bege." width={250} height={300}/>
+                    <p>Meia Cargo</p>
+                    <p>R$28,00</p>
+                    <p>Meia Cargo na cor branca com preta, estampada.</p>
+                    <button>Esgotado.</button>
+                </li>
+            </ul>
         </div>
     )
 }
