f0b1b9d6cc51caff8fed0d3592dbb7bd63673d61
gabsmelin <rm550202@fiap.com.br>
2023-10-18 19:26:16 -0300

    adicionando img da logo/mesendo no css do da page calça e no componente cabecalho.

--- src/Components/Cabecalho/page.jsx
+++ src/Components/Cabecalho/page.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function Cabecalho() {
     return (
         <div>
+            <Image src="/img/logo.png" alt="logo" width={65} height={60}/>
             <p><Link href="/">Home</Link></p>
             <p><Link href="/produtos/calca">Calças</Link></p>
             <p><Link  href="/produtos/tenis">Tênis</Link></p>
